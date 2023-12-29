e3d10eedd5468208bef0ad69047370f9076e7435
Nangkiller098 <hsomnang098@gmail.com>
2023-12-29 21:43:30 +0700

    setup ActivityDetails

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72928727b710a379962996b22bd67f86873133ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fafe7bed245cfe202f7d94b647ea8c15edfead50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid } from "semantic-ui-react";
 import { Activities } from "../../../models/Activities";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
 
 //create interface for get data activities
 interface Props {
@@ -13,6 +14,9 @@ export default function ActivityDashboard({ activities }: Props) {
     <Grid>
       <Grid.Column width={10}>
         <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width={"6"}>
+        <ActivityDetails activity={activities[0]} />
       </Grid.Column>
     </Grid>
   );
