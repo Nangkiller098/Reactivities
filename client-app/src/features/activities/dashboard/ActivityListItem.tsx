b82811bee98923afa943c8bf720477a3870d0361
heng somnang <hsomnang098@gmail.com>
2024-01-04 16:07:21 +0700

    clean up some error in client-app

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,24 +1,11 @@
-import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
+import { Button, Icon, Item,Segment } from "semantic-ui-react";
 import { Activities } from "../../../models/Activities";
 import { Link } from "react-router-dom";
-import { useStore } from "../../../app/stores/store";
-import { SyntheticEvent, useState } from "react";
 
 interface Props {
   activity: Activities;
 }
 export default function ActivityListItem({ activity }: Props) {
-  const { activityStore } = useStore();
-  const { deleteActivity, loading } = activityStore;
-  const [target, setTarget] = useState("");
-
-  function handleActivityDelete(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
   return (
     <Segment.Group>
       <Segment>
